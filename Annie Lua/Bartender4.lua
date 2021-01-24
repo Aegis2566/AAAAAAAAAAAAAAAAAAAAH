bf47270681e1b939954475604ddb003b8ee9ce11
Aegis2566 <43917777+Aegis2566@users.noreply.github.com>
2021-01-24 12:52:25 +0100

    Add files via upload

---
+++ Annie Lua/Bartender4.lua
@@ -0,0 +1,426 @@
+
+Bartender4DB = {
+	["namespaces"] = {
+		["StatusTrackingBar"] = {
+			["profiles"] = {
+				[""] = {
+					["enabled"] = true,
+					["version"] = 3,
+					["position"] = {
+						["scale"] = 1.264999985694885,
+						["x"] = -515,
+						["point"] = "BOTTOM",
+						["y"] = 62,
+					},
+				},
+				["AnnieFuchsia"] = {
+					["position"] = {
+						["y"] = 4,
+						["x"] = -405.499877929688,
+						["point"] = "TOP",
+					},
+					["version"] = 3,
+				},
+			},
+		},
+		["ActionBars"] = {
+			["profiles"] = {
+				[""] = {
+					["actionbars"] = {
+						{
+							["padding"] = 6,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 41.75,
+								["x"] = -510,
+								["point"] = "BOTTOM",
+							},
+						}, -- [1]
+						{
+							["enabled"] = false,
+							["version"] = 3,
+							["position"] = {
+								["y"] = -227.5000305175781,
+								["x"] = -231.5000915527344,
+								["point"] = "CENTER",
+							},
+						}, -- [2]
+						{
+							["rows"] = 12,
+							["padding"] = 5,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 610,
+								["x"] = -82,
+								["point"] = "BOTTOMRIGHT",
+							},
+						}, -- [3]
+						{
+							["rows"] = 12,
+							["padding"] = 5,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 610,
+								["x"] = -42,
+								["point"] = "BOTTOMRIGHT",
+							},
+						}, -- [4]
+						{
+							["padding"] = 6,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 110,
+								["x"] = 3,
+								["point"] = "BOTTOM",
+							},
+						}, -- [5]
+						{
+							["padding"] = 6,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 110,
+								["x"] = -510,
+								["point"] = "BOTTOM",
+							},
+						}, -- [6]
+						{
+						}, -- [7]
+						{
+						}, -- [8]
+						[10] = {
+						},
+					},
+				},
+				["AnnieFuchsia"] = {
+					["actionbars"] = {
+						{
+							["fadeoutdelay"] = 0,
+							["fadeoutalpha"] = 0.2,
+							["position"] = {
+								["y"] = 82.7573090564592,
+								["x"] = -443.973911985453,
+								["point"] = "BOTTOM",
+								["scale"] = 0.899999976158142,
+							},
+							["version"] = 3,
+							["padding"] = 5,
+						}, -- [1]
+						{
+							["version"] = 3,
+							["fadeoutalpha"] = 0.2,
+							["position"] = {
+								["y"] = 82.7573090564592,
+								["x"] = 0.626216843578732,
+								["point"] = "BOTTOM",
+								["scale"] = 0.899999976158142,
+							},
+							["padding"] = 5,
+						}, -- [2]
+						{
+							["version"] = 3,
+							["fadeoutalpha"] = 0.2,
+							["position"] = {
+								["y"] = 46.1048903649412,
+								["x"] = -444.322590565083,
+								["point"] = "BOTTOM",
+								["scale"] = 0.899999976158142,
+							},
+							["padding"] = 5,
+						}, -- [3]
+						{
+							["version"] = 3,
+							["fadeoutalpha"] = 0.2,
+							["position"] = {
+								["y"] = 44.0573100816591,
+								["x"] = 0.626216843578732,
+								["point"] = "BOTTOM",
+								["scale"] = 0.899999976158142,
+							},
+							["padding"] = 5,
+						}, -- [4]
+						{
+							["rows"] = 12,
+							["fadeoutdelay"] = 0.15,
+							["version"] = 3,
+							["fadeout"] = true,
+							["fadeoutalpha"] = 0.5,
+							["position"] = {
+								["y"] = -221.099853515625,
+								["x"] = -86.4998779296875,
+								["point"] = "TOPRIGHT",
+							},
+							["padding"] = 5,
+						}, -- [5]
+						{
+							["rows"] = 12,
+							["fadeout"] = true,
+							["version"] = 3,
+							["fadeoutalpha"] = 0.5,
+							["position"] = {
+								["y"] = -219.899719238281,
+								["x"] = -43.500244140625,
+								["point"] = "TOPRIGHT",
+							},
+							["padding"] = 5,
+						}, -- [6]
+						{
+							["rows"] = 2,
+							["enabled"] = true,
+							["buttons"] = 4,
+							["fadeoutalpha"] = 0,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 81.20381483491747,
+								["x"] = 528.859845428291,
+								["point"] = "BOTTOM",
+								["scale"] = 0.8999999761581421,
+							},
+							["padding"] = 5,
+							["visibility"] = {
+								["combat"] = false,
+								["always"] = false,
+							},
+						}, -- [7]
+						{
+							["rows"] = 12,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 248.553161621094,
+								["x"] = -506.479675292969,
+								["point"] = "CENTER",
+							},
+						}, -- [8]
+						{
+							["version"] = 3,
+							["position"] = {
+								["y"] = -227.499923706055,
+								["x"] = -231.500183105469,
+								["point"] = "CENTER",
+							},
+						}, -- [9]
+						{
+							["version"] = 3,
+							["position"] = {
+								["y"] = -227.500030517578,
+								["x"] = -231.500183105469,
+								["point"] = "CENTER",
+							},
+						}, -- [10]
+					},
+				},
+			},
+		},
+		["LibDualSpec-1.0"] = {
+		},
+		["ExtraActionBar"] = {
+			["profiles"] = {
+				[""] = {
+					["version"] = 3,
+					["position"] = {
+						["y"] = 287.0000305175781,
+						["x"] = -63.4998779296875,
+						["point"] = "BOTTOM",
+					},
+				},
+				["AnnieFuchsia"] = {
+					["position"] = {
+						["y"] = 258.536010742188,
+						["x"] = -63.9999885559082,
+						["point"] = "BOTTOM",
+					},
+					["version"] = 3,
+				},
+			},
+		},
+		["MicroMenu"] = {
+			["profiles"] = {
+				[""] = {
+					["padding"] = -2,
+					["version"] = 3,
+					["position"] = {
+						["scale"] = 1,
+						["x"] = 37.5,
+						["point"] = "BOTTOM",
+						["y"] = 41.75,
+					},
+				},
+				["AnnieFuchsia"] = {
+					["position"] = {
+						["y"] = 37.99999952316284,
+						["x"] = -277.3998584939764,
+						["point"] = "BOTTOMRIGHT",
+						["scale"] = 0.949999988079071,
+					},
+					["version"] = 3,
+					["padding"] = -2,
+				},
+			},
+		},
+		["ZoneAbilityBar"] = {
+			["profiles"] = {
+				["Alythee - Sylvanas"] = {
+					["version"] = 3,
+					["position"] = {
+						["y"] = 256.933319091797,
+						["x"] = 348.877563476563,
+						["point"] = "BOTTOM",
+					},
+				},
+			},
+		},
+		["XPBar"] = {
+			["profiles"] = {
+				["AnnieFuchsia"] = {
+					["enabled"] = true,
+					["version"] = 3,
+					["position"] = {
+						["y"] = 4.0001220703125,
+						["x"] = -518.499969482422,
+						["point"] = "TOP",
+					},
+				},
+			},
+		},
+		["APBar"] = {
+		},
+		["BlizzardArt"] = {
+			["profiles"] = {
+				[""] = {
+					["enabled"] = true,
+					["version"] = 3,
+					["position"] = {
+						["y"] = 47,
+						["x"] = -512,
+						["point"] = "BOTTOM",
+					},
+				},
+				["AnnieFuchsia"] = {
+					["position"] = {
+						["y"] = 47,
+						["x"] = -512,
+						["point"] = "BOTTOM",
+					},
+					["version"] = 3,
+				},
+			},
+		},
+		["Vehicle"] = {
+			["profiles"] = {
+				[""] = {
+					["version"] = 3,
+					["position"] = {
+						["y"] = 42.50015258789063,
+						["x"] = 104.4999389648438,
+						["point"] = "CENTER",
+					},
+				},
+				["AnnieFuchsia"] = {
+					["position"] = {
+						["y"] = 47.5000305175781,
+						["x"] = 99.5,
+						["point"] = "CENTER",
+					},
+					["version"] = 3,
+				},
+			},
+		},
+		["BagBar"] = {
+			["profiles"] = {
+				[""] = {
+					["version"] = 3,
+					["position"] = {
+						["y"] = 38.5,
+						["x"] = 345,
+						["point"] = "BOTTOM",
+					},
+				},
+				["AnnieFuchsia"] = {
+					["enabled"] = false,
+					["onebag"] = true,
+					["version"] = 3,
+					["position"] = {
+						["y"] = 19.5,
+						["x"] = 402.546142578125,
+						["point"] = "CENTER",
+					},
+				},
+			},
+		},
+		["StanceBar"] = {
+			["profiles"] = {
+				[""] = {
+					["version"] = 3,
+					["position"] = {
+						["y"] = -14.99997711181641,
+						["x"] = -82.50003051757812,
+						["point"] = "CENTER",
+					},
+				},
+				["AnnieFuchsia"] = {
+					["enabled"] = false,
+					["position"] = {
+						["y"] = 88.2680349398379,
+						["x"] = -464.864762846701,
+						["point"] = "BOTTOMRIGHT",
+						["scale"] = 1.29999995231628,
+					},
+					["version"] = 3,
+				},
+			},
+		},
+		["PetBar"] = {
+			["profiles"] = {
+				[""] = {
+					["version"] = 3,
+					["position"] = {
+						["y"] = 143,
+						["x"] = -460,
+						["point"] = "BOTTOM",
+					},
+				},
+				["AnnieFuchsia"] = {
+					["version"] = 3,
+					["position"] = {
+						["y"] = 124.099594116211,
+						["x"] = -402.441589355469,
+						["point"] = "BOTTOM",
+					},
+				},
+			},
+		},
+		["RepBar"] = {
+			["profiles"] = {
+				["AnnieFuchsia"] = {
+					["enabled"] = true,
+					["version"] = 3,
+					["position"] = {
+						["y"] = 91.8346632336775,
+						["x"] = -412.800136741003,
+						["point"] = "BOTTOM",
+						["scale"] = 0.800000011920929,
+					},
+				},
+			},
+		},
+	},
+	["profileKeys"] = {
+		["Anniefuchsia - Ravencrest"] = "AnnieFuchsia",
+	},
+	["profiles"] = {
+		[""] = {
+			["focuscastmodifier"] = false,
+			["blizzardVehicle"] = true,
+			["outofrange"] = "hotkey",
+		},
+		["AnnieFuchsia"] = {
+			["blizzardVehicle"] = true,
+			["focuscastmodifier"] = false,
+			["buttonlock"] = true,
+			["minimapIcon"] = {
+				["minimapPos"] = 147.002226343435,
+				["hide"] = true,
+			},
+			["outofrange"] = "hotkey",
+		},
+	},
+}
